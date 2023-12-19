b10e9762ae3de7e0795e2182ed71729087affa60
DomiBlack2k <101991724+DomiBlack2k@users.noreply.github.com>
2023-12-19 08:17:44 +0100

    changes Noise models

--- src/deep_reinforcement_learning/scripts/mirobot_learn_SAC.py
+++ src/deep_reinforcement_learning/scripts/mirobot_learn_SAC.py
@@ -8,7 +8,7 @@ from stable_baselines3.sac.policies import SACPolicy
 TIMESTEPS = 1000 # probably 100000
 EPISODES = 1000000   # probably auch so 1000 
 current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-MODELNAME = f"SAC_{current_time}_NormalNoise"
+MODELNAME = f"SAC_{current_time}_OrnsteinUhlenbeckNoise"
 ###
 
 models_dir = "drlsaves/models/"+MODELNAME
@@ -25,7 +25,7 @@ env.reset()
 print("[mirobot_env] environment: ", env)
 
 n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
 
 model = SAC(policy=SACPolicy, env=env, action_noise=action_noise, verbose = 1, tensorboard_log=logdir)
--- src/deep_reinforcement_learning/scripts/mirobot_learn_TD3.py
+++ src/deep_reinforcement_learning/scripts/mirobot_learn_TD3.py
@@ -8,7 +8,7 @@ from stable_baselines3.td3.policies import TD3Policy
 TIMESTEPS = 1000 # probably 10000
 EPISODES = 1000000   # probably auch so 1000 
 current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-MODELNAME = f"TD3_{current_time}_NormalNoise"
+MODELNAME = f"TD3_{current_time}-OrnsteinUhlenbeckNoise"
 ###
 
 models_dir = "drlsaves/models/"+MODELNAME
@@ -25,9 +25,9 @@ env.reset()
 print("[mirobot_env] environment: ", env)
 
 n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-model = TD3(policy=TD3Policy, env=env, action_noise=action_noise, batch_size=100, verbose = 1, tensorboard_log=logdir)
+model = TD3(policy=TD3Policy, env=env, action_noise=action_noise, verbose = 1, tensorboard_log=logdir)
 
 for i in range(1,EPISODES):
     model.learn(total_timesteps= TIMESTEPS, reset_num_timesteps= False, tb_log_name=MODELNAME)
